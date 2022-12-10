faa6d80ce8fba23ca8b2a11532da5d00839897d7
Aurelie <aurebes@gmail.com>
2022-12-10 10:57:47 +0100

    add success message and error message + post data

--- src/app/Pages/contact-page/contact-page.component.ts
+++ src/app/Pages/contact-page/contact-page.component.ts
@@ -1,6 +1,8 @@
 
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-page',
@@ -16,10 +18,13 @@ export class ContactPageComponent implements OnInit {
   public lastName: string = '';
   public message: string = '';
   public errorFields: boolean = false;
+  public successMessage: boolean = false;
 
   constructor(
+    private http: HttpClient,
+    private router: Router
   ) { 
-
+    
   }
 
   ngOnInit(): void {
@@ -35,14 +40,43 @@ export class ContactPageComponent implements OnInit {
 
   public submit(){
     if(this.contactForm.valid){
-      this.postDataFormContact()
+      this.postDataFormContact(this.contactForm.value)
     }else{
       this.errorFields = true
       console.log("no")
     }
 
   }
-  public postDataFormContact(){
-    console.log(this.contactForm.value)
+  // public postDataFormContact(){
+  //   console.log(this.contactForm.value)
+  //   this.http.post("http://localhost:3004/messages",{
+  //     firstName : this.contactForm.get('firstName'),
+  //     lastName : this.contactForm.get('lastName'),
+  //     phoneNumer : this.contactForm.get('phoneNumer'),
+  //     mail : this.contactForm.get('mail'),
+  //     message : this.contactForm.get('message'),
+  //   }, {
+  //     withCredentials: true,
+  //   })
+  //   .subscribe({
+  //     next: () => console.log("ok"),
+  //     error: (e) => console.log(e)
+  //   })
+
+  // }
+  public postDataFormContact(contactForm){
+    return fetch("http://localhost:3004/messages",{
+        method: 'POST',
+        body: JSON.stringify(contactForm),
+        headers: {'Content-Type':'application/json'},
+    })
+    .then(()=>this.successMessage = true)
+    .then(()=>this.contactForm.reset({
+      message: '',
+      firstName: '',
+      lastName: '',
+      phoneNumber:  '',
+      mail: ''
+  }))
   }
 }
