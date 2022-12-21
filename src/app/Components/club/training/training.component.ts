a47b8e02de11dbe8443be02339c6e9438e9f1c88
Aurelie <aurebes@gmail.com>
2022-12-21 22:48:07 +0100

    wip training

--- src/app/Components/club/training/training.component.ts
+++ src/app/Components/club/training/training.component.ts
@@ -9,17 +9,13 @@ import { TrainingService } from 'src/app/Services/training.service';
 export class TrainingComponent implements OnInit {
 
   public allTraining : any[]= [];
-  spMonday: any[]=[]
-  monday: any[]=[]
-  tuesday : any[]=[]
-  wednesday : any[]=[]
-  thursday: any[]=[]
-  friday: any[]=[]
-  saturday: any[]=[]
-  // public training : any[]= [];
-  // public trainingOld : any[]= [];
-  // public trainingYoung : any[]= [];
-  // public trainingMini : any[]= [];
+  public spMonday: any[]=[]
+  public monday: any[]=[]
+  public tuesday : any[]=[]
+  public wednesday : any[]=[]
+  public thursday: any[]=[]
+  public friday: any[]=[]
+  public saturday: any[]=[]
   public sg : boolean = false
   public sf : boolean = false
   public lois : boolean = false
@@ -39,9 +35,6 @@ export class TrainingComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllTraining()
-    // this.getTrainingBySection("mini");
-    // this.getTrainingBySection("old");
-    // this.getTrainingBySection("young");
   }
 public getAllTraining(){
   this.trainingService.getAllTraining()
@@ -71,36 +64,6 @@ public tri(){
     if(r.jour === "SA"){
       this.saturday.push(r)
     }
-   
   }
-  console.log(this.monday, 'monday', this.thursday, 'thr', this.saturday, 'sa')
 }
-  // public getTrainingBySection(section:string){
-  //   console.log("ici")
-  //   this.trainingService.getAllTraining()
-  //   .subscribe((r:[])=>{
-  //     this.training = r
-  //   if(section === 'mini'){
-  //     const datas:any[]= this.training.filter(function(d){
-  //       return d.section === "mini"
-  //     })
-  //     this.trainingMini = datas
-  //     console.log("h", this.trainingMini)
-  //   }else if(section === 'young'){
-  //     const datas:any[]= this.training.filter(function(d){
-  //       return d.section === "young"
-  //     })
-  //     this.trainingYoung = datas
-  //     console.log("y", this.trainingYoung)
-  //   }else if(section === 'old'){
-  //     const datas:any[]= this.training.filter(function(d){
-  //       return d.section === "old"
-  //     })
-  //     this.trainingOld = datas
-  //     console.log("o", this.trainingOld)
-  //   }
-  //   })
-    
-  // }
-
 }
