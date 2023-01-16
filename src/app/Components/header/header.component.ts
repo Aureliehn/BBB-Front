f3ae7c528f5cde4ba3c8a5ce03d60654d262d855
Aurelie <aurebes@gmail.com>
2023-01-16 17:15:05 +0100

    update header btn and table

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
-})
+})  
 export class HeaderComponent {
+  @ViewChild('bgVideo') bgVideo: ElementRef;
+  @ViewChild('bgImage') bgImage: ElementRef;
+  constructor(private renderer: Renderer2) {}
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.bgImage.nativeElement, 'display', 'none');
+    console.log(this.bgVideo);
+    this.bgVideo.nativeElement.addEventListener('loadeddata', () => {
+      this.bgVideo.nativeElement.addEventListener('ended', () => {
+        this.renderer.setStyle(this.bgVideo.nativeElement, 'display', 'none');
+        this.renderer.setStyle(this.bgImage.nativeElement, 'display', 'block');
+      });
+    });
+  }
 
 }
+
+
